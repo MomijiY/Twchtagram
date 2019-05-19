207b2aab94533e21711af2e027281943e46de8cf
吉川椛 <momiji@yoshikawanomacbook-pro.local>
2019-05-19 15:50:49 +0900

    Initial Commit

---
+++ TwchtagramTests/TwchtagramTests.swift
@@ -0,0 +1,34 @@
+//
+//  TwchtagramTests.swift
+//  TwchtagramTests
+//
+//  Created by 吉川椛 on 2019/05/19.
+//  Copyright © 2019 com.litech. All rights reserved.
+//
+
+import XCTest
+@testable import Twchtagram
+
+class TwchtagramTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
